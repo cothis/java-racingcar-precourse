2b0fd275a29b0b92d476a52afb23af6999acfc06
cothis <minho1030@naver.com>
2022-04-23 17:14:35 +0900

    feat(RacingGameController): 컨트롤러 및 Application 연결

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.view.RacingGameView;
+import racingcar.view.RacingGameViewImpl;
+import racingcar.view.RacingGameViewProxy;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGameView racingGameView = new RacingGameViewProxy(new RacingGameViewImpl());
+        new RacingGameController(racingGameView).start();
     }
 }
